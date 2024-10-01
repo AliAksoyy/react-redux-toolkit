fb764b62b2514a08b8c2d6cc01ee7f48a391d633
AliAksoyy <alifeyza4017@gmail.com>
2024-10-02 00:38:26 +0300

    added

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import BigSidebar from "./BigSidebar";
+import ChartsContainer from "./ChartsContainer";
+import FormRow from "./FormRow";
+import FormRowSelect from "./FormRowSelect";
+import Job from "./Job";
+import JobsContainer from "./JobsContainer";
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+import SearchContainer from "./SearchContainer";
+import SmallSidebar from "./SmallSidebar";
+import StatsContainer from "./StatsContainer";
+
+export {
+  Logo,
+  FormRow,
+  FormRowSelect,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  SearchContainer,
+  JobsContainer,
+  Job,
+  StatsContainer,
+  ChartsContainer,
+};
---
+++ src/features/allJobs/allJobsSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const allJobsSlice = createSlice({
+  name: "allJobs",
+  initialState,
+  reducers: {},
+});
+
+export const {} = allJobsSlice.actions;
+export default allJobsSlice.reducer;
